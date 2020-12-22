ed7cb488d050a4835336f3fc64b416de81764135
awjae <djwotmd@gmail.com>
2020-12-23 01:27:52 +0900

    20201222 signup form ing

--- client/src/component/User/SignUp.js
+++ client/src/component/User/SignUp.js
@@ -5,6 +5,10 @@ import './SignUp.css';
 function SignUp() {
 
     const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pw1, setPw1] = useState("");
+    const [pw2, setPw2] = useState("");
 
     const idCheckButton = useRef();
 
@@ -28,6 +32,10 @@ function SignUp() {
         })
 
     }
+
+    const habdleSetPw2 = (password) => {
+
+    }
     
     return (
         <section className="signUpPop">
@@ -36,10 +44,10 @@ function SignUp() {
             </header>
             <article className="signUpPop__input--wrapper">
                 <p><input placeholder="아이디" value={ id } onChange= {(e) => setId(e.target.value) }/><button ref={idCheckButton} className="signUpPop__checkId" onClick={ handleCheckId }>중복확인</button></p>
-                <p><input placeholder="닉네임" /></p>
-                <p><input placeholder="이메일" /></p>
-                <p className="signUpPop__input--pw"><input placeholder="비밀번호" /></p>
-                <p className="signUpPop__input--pw"><input placeholder="비밀번호 확인" /><span></span></p>
+                <p><input placeholder="닉네임" value={ name } onChange= {(e) => setName(e.target.value) }/></p>
+                <p><input placeholder="이메일" type="email" value={ email } onChange= {(e) => setEmail(e.target.value) }/></p>
+                <p className="signUpPop__input--pw"><input placeholder="비밀번호" type="password" value={ pw1 } onChange= {(e) => setPw1(e.target.value) }/></p>
+                <p className="signUpPop__input--pw"><input placeholder="비밀번호 확인" type="password" value={ pw2 } onChange= {(e) => habdleSetPw2(e.target.value) }/><span></span></p>
             </article>
             <footer className="signUpPop__footer">
                 <ul>
