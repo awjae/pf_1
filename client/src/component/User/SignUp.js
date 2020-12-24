0d9bb170001c82a74dc7849e95c6ad41e1f67775
awjae <djwotmd@gmail.com>
2020-12-24 11:37:51 +0900

    pw Check

--- client/src/component/User/SignUp.js
+++ client/src/component/User/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios'; 
 import './SignUp.css';
 
@@ -11,6 +11,8 @@ function SignUp() {
     const [pw2, setPw2] = useState("");
 
     const idCheckButton = useRef();
+    const pw1CheckSpan = useRef();
+    const pw2CheckSpan = useRef();
 
     const handleCheckId = () => {
         idCheckButton.current.innerText = "확인중";
@@ -32,10 +34,19 @@ function SignUp() {
         })
 
     }
+    useEffect(() => {
+        if (pw1 !== "") {
+            pw1.length < 5 ? pw1CheckSpan.current.innerText = "5자 이상 입력해주세요." : pw1CheckSpan.current.innerText = "사용가능합니다.";
+        }
+        
 
-    const habdleSetPw2 = (password) => {
+    }, [pw1])
 
-    }
+    useEffect(() => {
+        if (pw1 !== "") {
+            pw1 === pw2 ? pw2CheckSpan.current.innerText = "사용가능합니다." : pw2CheckSpan.current.innerText = "비밀번호가 일지하지 않습니다.";
+        }
+    }, [pw2])
     
     return (
         <section className="signUpPop">
@@ -46,8 +57,8 @@ function SignUp() {
                 <p><input placeholder="아이디" value={ id } onChange= {(e) => setId(e.target.value) }/><button ref={idCheckButton} className="signUpPop__checkId" onClick={ handleCheckId }>중복확인</button></p>
                 <p><input placeholder="닉네임" value={ name } onChange= {(e) => setName(e.target.value) }/></p>
                 <p><input placeholder="이메일" type="email" value={ email } onChange= {(e) => setEmail(e.target.value) }/></p>
-                <p className="signUpPop__input--pw"><input placeholder="비밀번호" type="password" value={ pw1 } onChange= {(e) => setPw1(e.target.value) }/></p>
-                <p className="signUpPop__input--pw"><input placeholder="비밀번호 확인" type="password" value={ pw2 } onChange= {(e) => habdleSetPw2(e.target.value) }/><span></span></p>
+                <p className="signUpPop__input--pw"><input placeholder="비밀번호" type="password" value={ pw1 } onChange= {(e) => setPw1(e.target.value) }/><span ref={ pw1CheckSpan }></span></p>
+                <p className="signUpPop__input--pw"><input placeholder="비밀번호 확인" type="password" value={ pw2 } onChange= {(e) => setPw2(e.target.value) }/><span ref={ pw2CheckSpan }></span></p>
             </article>
             <footer className="signUpPop__footer">
                 <ul>
