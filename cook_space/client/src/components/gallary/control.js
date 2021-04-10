1dda68364b2b77404f7e29ce4e37fdbb95b7dcec
awjae <djwotmd@gmail.com>
2021-04-10 14:54:34 +0900

    add mouse wheel

--- cook_space/client/src/components/gallary/control.js
+++ cook_space/client/src/components/gallary/control.js
@@ -8,16 +8,17 @@ control.init = (camera, renderer) => {
     // controls
     const controls = new OrbitControls( camera, renderer.domElement );
     controls.maxPolarAngle = Math.PI * 0.45;
-    controls.minDistance = 5;
-    controls.maxDistance = 3;
-    controls.dampingFactor = 0.05; // friction
+    // controls.minDistance = 5;
+    // controls.maxDistance = 3;
+    controls.dampingFactor = 0.25; // friction
     controls.rotateSpeed = 1; // mouse sensitivity
     controls.panSpeed = 1;
-    // controls.enableDamping = true;
+    controls.zoomSpeed = 1.2;
     // controls.enablePan = false;
+    // smooth Zoom
 
 
-
+    control.curr = controls;
     return control.curr;
 }
 
--- cook_space/client/src/pages/gallary.js
+++ cook_space/client/src/pages/gallary.js
@@ -45,6 +45,7 @@ const gallary = () => {
         scene.add(dirLight);
         scene.background = new THREE.Color(0x282c34);
 
+        const controls = controller.init(camera, renderer);
         //랜더링 갱신
         const animate = () => {
             requestAnimationFrame( animate );
@@ -53,82 +54,11 @@ const gallary = () => {
             cube.rotation.y += 0.01;
 
             renderer.render( scene, camera );
+            controls.update();
         }
-        controller.init(camera, renderer);
+        
         animate();
 
-        // let width = container.current.clientWidth;
-        // let height = container.current.clientHeight;
-        // let frameId;
-
-        // const scene = new THREE.Scene();
-        // const camera = new THREE.PerspectiveCamera(75,  width / height, 0.1, 1000);
-        // const renderer = new THREE.WebGLRenderer({
-        //     antialias: true
-        // });
-        // const geometry = new THREE.BoxGeometry(1, 1, 1);
-        // const material = new THREE.MeshBasicMaterial({
-        //     color: 0xff00ff
-        // });
-        // const cube = new THREE.Mesh(geometry, material);
-
-        // camera.position.z = 4;
-        // scene.add(cube);
-        // renderer.setClearColor('#000000');
-        // renderer.setSize(width, height);
-
-        // const renderScene = () => {
-        //     renderer.render(scene, camera);
-        // }
-
-        // const handleResize = () => {
-        //     width = container.current.clientWidth;
-        //     height = container.current.clientHeight;
-        //     renderer.setSize(width, height);
-        //     camera.aspect = width / height;
-        //     camera.updateProjectionMatrix();
-        //     renderScene();
-        // }
-
-        // const animate = () => {
-        //     cube.rotation.x += 0.01;
-        //     cube.rotation.y += 0.01;
-
-        //     renderScene();
-        //     frameId = window.requestAnimationFrame(animate);
-        // }
-
-        // const start = () => {
-        //     if (!frameId) {
-        //         frameId = requestAnimationFrame(animate);
-        //     }
-        // }
-
-        // const stop = () => {
-        //     cancelAnimationFrame(frameId);
-        //     frameId = null;
-        // }
-
-        // container.current.appendChild(renderer.domElement);
-        // window.addEventListener('resize', handleResize);
-        // start();
-
-        // controls.current = {
-        //     start,
-        //     stop
-        // }
-
-        // floor.init(scene);
-
-        // return () => {
-        //     stop();
-        //     window.removeEventListener('resize', handleResize);
-        //     container.current.removeChild(renderer.domElement);
-
-        //     scene.remove(cube);
-        //     geometry.dispose();
-        //     material.dispose();
-        // }
     }, [])
 
     return (
