88fe2612b1aba371a311fba0682c0f0b76917716
awjae <djwotmd@gmail.com>
2021-04-19 21:08:49 +0900

    image loader

---
+++ cook_space/client/src/components/gallary/userImage.js
@@ -0,0 +1,14 @@
+import * as THREE from "three";
+
+
+const userImage = {};
+
+userImage.init = () => {
+
+    const loader = new THREE.TextureLoader();
+    
+
+    return userImage.curr;
+}
+
+export default userImage;
