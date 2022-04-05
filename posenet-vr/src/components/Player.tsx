a2693cdc7f2b6be110c12c842d63accc21945fb3
awjae <djwotmd@gmail.com>
2022-04-05 21:47:53 +0900

    스켈레톤 추가

--- posenet-vr/src/components/Player.tsx
+++ posenet-vr/src/components/Player.tsx
@@ -12,6 +12,24 @@ function Player() {
   let ctx = null;
   let detector = null;
   let model = null
+  const modelTree = {
+    "0": "1",
+    "1": "3",
+    "2": "0",
+    "4": "2",
+    "5": "11",
+    "6": "5",
+    "7": "5",
+    "8": "6",
+    "9": "7",
+    "10": "8",
+    "11": "12",
+    "12": "6",
+    "13": "11",
+    "14": "12",
+    "15": "13",
+    "16": "14",
+  }
 
   async function raf() {
 
@@ -22,32 +40,39 @@ function Player() {
         };
       });
     }
-    
-    
+
     const poses = await detector.estimatePoses(videoEl.current);
     ctx.drawImage(videoEl.current, 0, 0, videoEl.current.videoWidth, videoEl.current.videoHeight);
-    poses[0]?.keypoints.forEach(el => {
+    poses[0]?.keypoints.forEach((el, idx) => {
       if (el.score >= 0.3) {
         const circle = new Path2D();
         circle.arc(el.x, el.y, 4, 0, 2 * Math.PI);
         ctx.fill(circle);
+        ctx.fillStyle = "#00ff2a";
+        ctx.strokeStyle = "#00ff2a";
         ctx.stroke(circle);
+        if (modelTree.hasOwnProperty(String(idx)) && poses[0].keypoints[parseInt(modelTree[String(idx)])].score >= 0.3) {
+          ctx.beginPath();
+          ctx.strokeStyle = '#fff';
+          ctx.moveTo(poses[0].keypoints[idx].x, poses[0].keypoints[idx].y);
+          ctx.lineTo(poses[0].keypoints[modelTree[String(idx)]].x, poses[0].keypoints[modelTree[String(idx)]].y);
+          ctx.stroke();
+        }
       }
     })
+
     requestAnimationFrame(() => raf());
   }
 
   async function checkCamera() {
     model = poseDetection.SupportedModels.MoveNet;
     detector = await poseDetection.createDetector(model);
-    
     raf();
   }
 
   useEffect(function () {
     if (output.current !== null) {
       ctx = output.current.getContext('2d');
-      ctx.fillStyle = "#00ff2a";
     }
 
     if (navigator.mediaDevices.getUserMedia) {
