5530bcad16cde1b342f80803b6a3359365a0695a
awjae <djwotmd@gmail.com>
2022-04-07 19:11:21 +0900

    set three

--- posenet-vr/src/components/Character.tsx
+++ posenet-vr/src/components/Character.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import React, { useEffect, useRef } from 'react'
+import styled from '@emotion/styled';
+import * as THREE from 'three';
 
 function Character() {
+  const wrapper = useRef(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 45, 1, 1, 500 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 500, 500 );
+    wrapper.current.appendChild( renderer.domElement );
+
+    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    const points = [];
+    points.push( new THREE.Vector3( - 10, 0, 0 ) );
+    points.push( new THREE.Vector3( 0, 10, 0 ) );
+    points.push( new THREE.Vector3( 10, 0, 0 ) );
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const line = new THREE.Line( geometry, material );
+    camera.position.set( 0, 0, 100 );
+    camera.lookAt( 0, 0, 0 );
+    scene.add( line );
+    renderer.render( scene, camera );
+
+    
+  })
+
   return (
-    <CharacterDiv></CharacterDiv>
+    <CharacterDiv ref={wrapper}>
+    </CharacterDiv>
   )
 }
 
